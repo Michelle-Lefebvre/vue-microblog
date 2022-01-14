7714ec5f513f9209aa30208395ca5e99860893df
Michelle <michelle.lefebvre@valtech.com>
2022-01-14 10:39:29 -0500

    initial setup for photos project and reusable layout

---
+++ photos/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <layout>
+    <template v-slot:header>Header</template>
+    <template v-slot:sidebar>sidebar</template>
+    <template v-slot:content>content</template>
+  </layout>
+</template>
+
+<script>
+import Layout from './Layout.vue';
+
+export default {
+  components: {
+    Layout
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+}
+</style>
---
+++ photos/store.js
@@ -0,0 +1,16 @@
+import {
+  createStore
+} from 'vuex'
+import {
+  albums
+} from './albums'
+import {
+  photos
+} from './photos'
+
+export const store = createStore({
+  modules: { // register
+    albums,
+    photos
+  }
+})
